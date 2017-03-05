ee525399ab0fa355df2a4b9fa5359d99a217ff45
erickao08 <erickao08@hotmail.com>
2017-03-05 14:12:58 -0600

    combine address

--- data_wrangling_ex_1.R
+++ data_wrangling_ex_1.R
@@ -10,10 +10,15 @@ library(stringr)
 product_code_name<-str_split_fixed(refine_df$Product.code...number, "-", 2)
 refine_df['Product.code']<-product_code_name[,1]
 refine_df['product_number']<-product_code_name[,2]
+
 #add categories
 refine_df$prodcut_category[refine_df$Product.code=='x']<-'Laptop'
 refine_df$prodcut_category[refine_df$Product.code=='p']<-'Smartphone'
 refine_df$prodcut_category[refine_df$Product.code=='v']<-'TV'
 refine_df$prodcut_category[refine_df$Product.code=='q']<-'Tablet' 
+
+#Add full address for geocoding
+refine_df$full_address = paste (refine_df$address,refine_df$city,refine_df$country, sep = ", ", collapse = NULL)
+
 
 
