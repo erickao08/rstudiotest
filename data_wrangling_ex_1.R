808e19b247bf2079350522b2412b6336fa7b489c
erickao08 <erickao08@hotmail.com>
2017-03-05 12:14:40 -0600

    add comment 03052017

--- data_wrangling_ex_1.R
+++ data_wrangling_ex_1.R
@@ -1,21 +1,19 @@
-
+#read profile
 library(dplyr)
 refine<-read.csv('refine_original.csv')
 refine_df<-tbl_df(refine)
 View(refine_df)
 glimpse(refine_df)
 
-
+#seperate the code and name 
 library(stringr)
 product_code_name<-str_split_fixed(refine_df$Product.code...number, "-", 2)
 refine_df['Product.code']<-product_code_name[,1]
 refine_df['product_number']<-product_code_name[,2]
+#add categories
 refine_df$prodcut_category[refine_df$Product.code=='x']<-'Laptop'
 refine_df$prodcut_category[refine_df$Product.code=='p']<-'Smartphone'
 refine_df$prodcut_category[refine_df$Product.code=='v']<-'TV'
-refine_df$prodcut_category[refine_df$Product.code=='q']<-'Tablet'
+refine_df$prodcut_category[refine_df$Product.code=='q']<-'Tablet' 
 
-p = Smartphone
-v = TV
-x = Laptop
-q = Tablet
+
