4c6e25a2fb827b88b591500208d693cf42ed00f5
erickao08 <erickao08@hotmail.com>
2017-03-05 12:12:14 -0600

    Add product category

--- data_wrangling_ex_1.R
+++ data_wrangling_ex_1.R
@@ -10,3 +10,12 @@ library(stringr)
 product_code_name<-str_split_fixed(refine_df$Product.code...number, "-", 2)
 refine_df['Product.code']<-product_code_name[,1]
 refine_df['product_number']<-product_code_name[,2]
+refine_df$prodcut_category[refine_df$Product.code=='x']<-'Laptop'
+refine_df$prodcut_category[refine_df$Product.code=='p']<-'Smartphone'
+refine_df$prodcut_category[refine_df$Product.code=='v']<-'TV'
+refine_df$prodcut_category[refine_df$Product.code=='q']<-'Tablet'
+
+p = Smartphone
+v = TV
+x = Laptop
+q = Tablet
