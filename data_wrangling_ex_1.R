647c53ba1f49eb005bf616535da5c1792c94df9a
erickao08 <erickao08@hotmail.com>
2017-03-04 18:24:50 -0600

    seperated product_code and product_name

--- data_wrangling_ex_1.R
+++ data_wrangling_ex_1.R
@@ -3,3 +3,10 @@ library(dplyr)
 refine<-read.csv('refine_original.csv')
 refine_df<-tbl_df(refine)
 View(refine_df)
+glimpse(refine_df)
+
+
+library(stringr)
+product_code_name<-str_split_fixed(refine_df$Product.code...number, "-", 2)
+refine_df['Product.code']<-product_code_name[,1]
+refine_df['product_number']<-product_code_name[,2]
